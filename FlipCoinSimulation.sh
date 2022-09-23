a93361c504a8326d46b3d3c5cd34248774d29be8
sambit420 <sambit2024@gmail.com>
2022-09-23 10:34:32 +0530

    Added in : Flipping a Coin Multiple Times & show No of times Head and Tail has won

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,7 +1,47 @@
-toss=$(( RANDOM % 2 ))
-if(( toss == 0 ))
+read -p "Enter no of times to toss: " n
+
+heads_count=0
+tails_count=0
+
+for(( i = 1; i <= n; i++ ))
+
+do
+
+    toss=$(( RANDOM % 2 ))
+    if(( toss == 0 ))
+
+    then
+
+        echo "Flip-$i Heads"
+        (( heads_count++ ))
+
+    else
+
+        echo "Flip-$i Tails"
+        (( tails_count++ ))
+
+    fi 
+
+done
+
+echo "The Heads count is $heads_count" 
+
+echo "Tails Count is $tails_count"
+
+if(( heads_count > tails_count ))
+
 then
-    echo "Coin flips Heads"
-else
-    echo "Coin flips Tails"
+
+    echo "Winner is Heads"
+
+elif(( tails_count > heads_count ))
+
+then
+
+    echo "Winner is Tails"
+
+else    
+
+    echo "It is tie"
+
 fi
